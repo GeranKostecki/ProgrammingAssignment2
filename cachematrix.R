c73b26dfa7d2d21bc977f26b8082192fdd9645ef
Geran Kostecki <48740156+GeranKostecki@users.noreply.github.com>
2019-04-02 21:30:13 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Uses caching to speed up solving the inverse of a set of matrices
 
-## Write a short comment describing this function
+## Create a structure of cached inverses
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    xr <- NULL
+    set <- function(y) {
+        x <<- y
+        xr <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) xr <<- inv
+    getinv <- function() xr
+    list(set = set, get = get,
+    setinv = setinv,
+    getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## solve new matrices
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    xr <- x$getinv()
+    if(!is.null(xr)) {
+        message("getting cached data")
+        return(xr)
+    }
+    data <- x$get()
+    xr <- solve(data, ...)
+    x$setinv(xr)
+    xr
 }
